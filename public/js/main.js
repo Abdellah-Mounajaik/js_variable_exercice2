a576f53c5044fd2ebfef892af4fb1cefdd5fc733
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 00:33:11 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,23 @@
+let manger = "j'aime les tartines et j'en mange";
+console.log(manger);
+let repetition = "4 par jours";
+console.log(repetition);
+console.log(manger + " " + repetition);
+let number1 = 5;
+let number2 = 35;
+console.log(number1 + number2);
+console.log(manger + " " + number1);
+let phrase1 = "Salut tout le monde"
+console.log(phrase1);
+let phrase2 = "Aurevoir !"
+console.log(phrase2);
+phrase1 = phrase2
+console.log(phrase1);
+let number3 = "4";
+let number4 = "6";
+console.log(number3 > number4); 
+console.log(number3 < number4); 
+console.log(number3 == number4); 
+console.log(number3 != number4); 
+console.log(number3 === number4); 
+console.log(number3 !== number4); 
